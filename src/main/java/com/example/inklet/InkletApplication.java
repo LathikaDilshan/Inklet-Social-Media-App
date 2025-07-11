93c3047f7bfdd74be941a4a0ef1e5c2501a3b557
Lathika Dilshan <133035674+LathikaDilshan@users.noreply.github.com>
2025-07-11 14:43:35 +0530

    Initialized Spring Boot project with required dependencies Project Name : "inklet" social media app

---
+++ src/main/java/com/example/inklet/InkletApplication.java
@@ -0,0 +1,13 @@
+package com.example.inklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inklet/InkletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inklet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
